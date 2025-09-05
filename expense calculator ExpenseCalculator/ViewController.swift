50c4d920d0866efa9cb55a6813699994b0d97cc3
Olga Vasileva <oi.vasileva@vk.team>
2025-09-06 00:40:27 +0400

    Initial Commit

---
+++ expense calculator ExpenseCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  expense calculator ExpenseCalculator
+//
+//  Created by Olga Vasileva on 06.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
